4d7a4cfe828057c2a3ea04a783520f32d67cdb97
joee63737 <egybet2@gmail.com>
2024-08-05 03:20:23 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,23 +3,23 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "923192173398"
-global.ownername = "🐛 MR WASI "
+global.ownernomer = "201012606541"
+global.ownername = "ليو مافيا"
 global.ytname = "YT: Dont have"
 global.socialm = "GitHub: private"
 global.location = "Pakistan, Lahore"
 
-global.ownernumber = '923347748038'  //creator number
-global.ownername = ' MR WASI ' //owner name
-global.botname = 'WASI TECH BUG BOT ' //name of the bot
+global.ownernumber = '201012606541'  //creator number
+global.ownername = ' ليو مافيا ' //owner name
+global.botname = 'LEO BOT ' //name of the bot
 
 //sticker details
 global.packname = 'Sticker By'
-global.author = 'WASI'
+global.author = 'LEO'
 
 //console view/theme
 global.themeemoji = '🪀'
-global.wm = "WASI TECH"
+global.wm = "LEO"
 
 //theme link
 global.link = 'https://whatsapp.com'
